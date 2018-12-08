0c3a15b0ad7bda502653127f8efe53a4a842ac6b
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2018-12-08 12:39:36 +0000

    remove nullability leftover

--- KotlinAdvent2018/week2/twinject/src/main/kotlin/net/twisterrob/challenges/adventOfKotlin2018/week2/DI.kt
+++ KotlinAdvent2018/week2/twinject/src/main/kotlin/net/twisterrob/challenges/adventOfKotlin2018/week2/DI.kt
@@ -5,24 +5,19 @@ import kotlin.reflect.KProperty
 
 class Twinject {
 
-	data class Type(
-		private val type: KClass<*>,
-		private val nullable: Boolean
-	)
-
 	inline operator fun invoke(block: Twinject.() -> Unit): Twinject = this.apply(block)
 
-	private val locator = mutableMapOf<Type, () -> Any?>()
+	private val locator = mutableMapOf<KClass<*>, () -> Any>()
 
 	inline fun <reified T : Any> register(noinline provider: () -> T) = register(T::class, provider)
 
 	fun <T : Any> register(type: KClass<T>, provider: () -> T) {
-		locator[Type(type, false)] = provider
+		locator[type] = provider
 	}
 
 	@Suppress("UNCHECKED_CAST")
 	operator fun <T : Any> get(type: KClass<T>): T =
-		locator.getValue(Type(type, false))() as T
+		locator.getValue(type)() as T
 
 	inline fun <reified T : Any> inject(): T = get(T::class)
 
