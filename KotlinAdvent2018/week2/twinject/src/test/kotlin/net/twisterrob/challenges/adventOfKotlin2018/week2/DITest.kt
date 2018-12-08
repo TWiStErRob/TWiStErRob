b623107bd9c7b33447afc6a0a122ebdc3af28a40
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2018-12-08 13:03:42 +0000

    Add some tests

--- KotlinAdvent2018/week2/twinject/src/test/kotlin/net/twisterrob/challenges/adventOfKotlin2018/week2/DITest.kt
+++ KotlinAdvent2018/week2/twinject/src/test/kotlin/net/twisterrob/challenges/adventOfKotlin2018/week2/DITest.kt
@@ -1,10 +1,66 @@
 package net.twisterrob.challenges.adventOfKotlin2018.week2
 
 import org.junit.jupiter.api.Test
+import kotlin.test.assertNotNull
 import kotlin.test.assertNotSame
 import kotlin.test.assertSame
+import kotlin.test.assertTrue
 
 class DITest {
+
+	@Test fun `inject provided class`() {
+		class Dependency
+
+		val inject = (Twinject()){
+			register { Dependency() }
+		}
+
+		@Suppress("UNUSED_VARIABLE")
+		val dependency: Dependency = inject()
+
+		// it compiles and runs (doesn't throw), that's the verification
+	}
+
+	@Test fun `inject provided object to nullable`() {
+		class Dependency
+
+		val inject = (Twinject()){
+			register { Dependency() }
+		}
+
+		val dependency: Dependency? = inject()
+
+		assertNotNull(dependency)
+	}
+
+	@Test fun `inject provided class as contract`() {
+		abstract class Contract
+		class Impl : Contract()
+
+		val inject = (Twinject()){
+			register<Contract> { Impl() }
+		}
+
+		val dependency: Contract = inject()
+
+		assertTrue(dependency is Impl)
+	}
+
+	@Test fun `inject by field injection`() {
+		class Dependency
+		class Dependent(inject: Twinject) {
+			val dep: Dependency by inject
+		}
+
+		val inject = (Twinject()){
+			register { Dependency() }
+			register { Dependent(this) }
+		}
+
+		val dependency: Dependent = inject()
+
+		dependency.dep
+	}
 
 	@Test fun `register singleton produces the same value`() {
 		class Dependency
