d9b89d624fd43f2476d4cf4f66a4354caafac5fc
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2018-12-14 01:06:21 +0000

    Hacky solution (iterator doesn't sort)

--- KotlinAdvent2018/week3/src/main/kotlin/net/twisterrob/challenges/adventOfKotlin2018/week3/SortedMutableList.kt
+++ KotlinAdvent2018/week3/src/main/kotlin/net/twisterrob/challenges/adventOfKotlin2018/week3/SortedMutableList.kt
@@ -1,17 +1,22 @@
 package net.twisterrob.challenges.adventOfKotlin2018.week3
 
-interface SortedMutableList<T> : Iterable<T> {
-	val size: Int
-	fun add(element: T)
-	fun remove(element: T)
-	operator fun get(index: Int): T
-	operator fun contains(element: T): Boolean
-}
+//interface SortedMutableList<T> : Iterable<T> {
+//	val size: Int
+//	fun add(element: T)
+//	fun remove(element: T)
+//	operator fun get(index: Int): T
+//	operator fun contains(element: T): Boolean
+//}
+
+typealias SortedMutableList<T> = java.util.PriorityQueue<T>
+
+operator fun <T : Comparable<T>> SortedMutableList<T>.get(index: Int): T =
+	asSequence().sortedWith(comparator() ?: naturalOrder<T>()).elementAt(index)
 
 fun <T : Comparable<T>> sortedMutableListOf(vararg elements: T): SortedMutableList<T> {
-	TODO()
+	return SortedMutableList(elements.asList())
 }
 
 fun <T> sortedMutableListOf(comparator: Comparator<T>, vararg elements: T): SortedMutableList<T> {
-	TODO()
+	return SortedMutableList(elements.size + 1, comparator).apply { addAll(elements) }
 }
