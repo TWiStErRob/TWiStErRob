4889958836e72adfecdbc3193ad7a26ba5772642
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2020-04-18 23:04:23 +0100

    LeetCode 2020 April: Week 3 Valid Parenthesis String (backtracking accepted)

--- LeetCode2020April/week3/src/main/kotlin/net/twisterrob/challenges/leetcode2020april/week3/valid_parenthesis/Solution.kt
+++ LeetCode2020April/week3/src/main/kotlin/net/twisterrob/challenges/leetcode2020april/week3/valid_parenthesis/Solution.kt
@@ -4,32 +4,48 @@ class Solution {
 	/**
 	 * @param s length `[1, 100]`, valid characters = `()*`
 	 */
-	fun checkValidString(s: String): Boolean {
-		var acc = 0
-		var jokers = 0
-		for (curr in s) {
-			when (curr) {
-				'(' -> acc += 1
-				')' -> acc -= 1
-				'*' -> jokers += 1
-				else -> error("invalid character $curr")
-			}
-			// violates 3.
-			if (acc < 0) {
-				if (jokers > 0) {
-					// but we can recover by using * as opening because of 4.
-					jokers -= 1
-					acc += 1
-				} else {
-					return false
-				}
+	fun checkValidString(s: String): Boolean =
+		try {
+			checkValidString(s, 0, 0)
+			false
+		} catch (ex: Success) {
+			true
+		}
+
+	private class Success : Exception()
+
+	private fun checkValidString(s: String, pos: Int, count: Int) {
+		// 3. Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+		if (count < 0) return
+
+		// If we reached the end, must make a decision:
+		if (pos == s.length) {
+			// if parentheses are balanced, we're good,
+			if (count == 0)
+				throw Success()
+			// otherwise this failed, backtrack.
+			else
+				return
+		}
+
+		when (s[pos]) {
+			// 1. Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+			'(' ->
+				checkValidString(s, pos + 1, count + 1)
+
+			// 2. Any right parenthesis ')' must have a corresponding left parenthesis '('.
+			')' ->
+				checkValidString(s, pos + 1, count - 1)
+
+			// 4. '*' could be treated as
+			'*' -> {
+				// a single right parenthesis ')'
+				checkValidString(s, pos + 1, count - 1)
+				// or a single left parenthesis '('
+				checkValidString(s, pos + 1, count + 1)
+				// or an empty string.
+				checkValidString(s, pos + 1, count)
 			}
 		}
-		while (acc > 0 && jokers > 0) {
-			// close any remaining parentheses by 4.
-			acc -= 1
-			jokers -= 1
-		}
-		return acc == 0
 	}
 }
