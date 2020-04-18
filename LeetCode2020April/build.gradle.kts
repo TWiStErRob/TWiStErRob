56f1853aa963faa41e5dbae967d7a2458c0ff0ac
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2020-04-18 17:56:26 +0100

    LeetCode 2020 April: enable Kotlin 1.4 support

--- LeetCode2020April/build.gradle.kts
+++ LeetCode2020April/build.gradle.kts
@@ -42,4 +42,10 @@ subprojects {
 		useJUnitPlatform()
 		testLogging.events(/*"passed",*/ "skipped", "failed")
 	}
+
+	tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+		kotlinOptions {
+			languageVersion = "1.4"
+		}
+	}
 }
