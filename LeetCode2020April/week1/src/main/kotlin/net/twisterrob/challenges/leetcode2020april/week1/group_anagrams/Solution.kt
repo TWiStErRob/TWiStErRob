6ed76bdba0a48fe8029b4ba5b092a605f1751e79
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2020-04-19 14:42:22 +0100

    LeetCode 2020 April: Week 1 Group Anagrams (extract key fun, thanks santa4nt)

--- LeetCode2020April/week1/src/main/kotlin/net/twisterrob/challenges/leetcode2020april/week1/group_anagrams/Solution.kt
+++ LeetCode2020April/week1/src/main/kotlin/net/twisterrob/challenges/leetcode2020april/week1/group_anagrams/Solution.kt
@@ -2,5 +2,10 @@ package net.twisterrob.challenges.leetcode2020april.week1.group_anagrams
 
 class Solution {
 	fun groupAnagrams(strs: Array<String>): List<List<String>> =
-		strs.groupBy { String(it.toCharArray().sortedArray()) }.values.toList()
+		strs.groupBy(::anagramSignature).values.toList()
+
+	companion object {
+		private fun anagramSignature(word: String): String =
+			String(word.toCharArray().sortedArray())
+	}
 }
