d553cbadcc0e1460c2e0c6ac9f32ecea55283f77
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2020-04-19 15:10:02 +0100

    LeetCode 2020 April: Week 1 Move Zeroes (don't copy)

--- LeetCode2020April/week1/src/main/kotlin/net/twisterrob/challenges/leetcode2020april/week1/move_zeroes/Solution.kt
+++ LeetCode2020April/week1/src/main/kotlin/net/twisterrob/challenges/leetcode2020april/week1/move_zeroes/Solution.kt
@@ -2,10 +2,18 @@ package net.twisterrob.challenges.leetcode2020april.week1.move_zeroes
 
 class Solution {
 	fun moveZeroes(nums: IntArray) {
-		val nonZero = nums.filterNot { it == 0 }.toIntArray()
-		// nums[beginning] = nonZero[..]
-		System.arraycopy(nonZero, 0, nums, 0, nonZero.size)
-		// nums[after nonZero] = 0
-		nums.indices.drop(nonZero.size).forEach { nums[it] = 0 }
+		var targetIndex = 0
+
+		// compact non-zero numbers at the beginning
+		nums
+			.asSequence()
+			.filterNot { it == 0 }
+			.forEach { n -> nums[targetIndex++] = n }
+
+		// fill end with zeroes
+		nums
+			.indices
+			.drop(targetIndex)
+			.forEach { zeroIndex -> nums[zeroIndex] = 0 }
 	}
 }
